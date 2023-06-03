dcf72aec0399417e5a6757f90290d6a8aaeb7492
JoinerTim <nguyendinhnghiaqwerty@gmail.com>
2023-06-03 18:21:09 +0700

    upload comit

--- src/util/upload.ts
+++ src/util/upload.ts
@@ -1,21 +1,16 @@
-import multer from "multer";
-import * as fs from "fs";
+import multer, { diskStorage } from "multer";
+import fs from "fs";
 import "reflect-metadata";
 import { Request } from "express";
-import { uuid } from "uuidv4";
-import { BadRequest } from "@tsed/exceptions"
+import { v4 as uuid } from "uuid";
+import { BadRequest } from "@tsed/exceptions";
 
-
-export const storage = multer.diskStorage({
-    destination: (req: any, file, cb) => {
+const storage: any = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb) => {
         const pathFinal = `./uploads`;
-
-        fs.mkdirSync(pathFinal, { recursive: true });
-
-        cb(null, pathFinal);
     },
-    filename: (req, file, cb) => {
-        var fileExtension = file.originalname.substr(
+    filename: (req: Request, file: Express.Multer.File, cb) => {
+        const fileExtension = file.originalname.substr(
             file.originalname.lastIndexOf(".") + 1
         );
         const uniqueSuffix = uuid() + "." + fileExtension;
@@ -23,7 +18,7 @@ export const storage = multer.diskStorage({
     },
 });
 
-export const fileFilter = (req: Request, file: any, cb: any) => {
+const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
     if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
         cb(null, true);
     } else {
@@ -31,4 +26,6 @@ export const fileFilter = (req: Request, file: any, cb: any) => {
     }
 };
 
-export const upload = multer({ storage: storage, fileFilter: fileFilter });
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+export { upload };
